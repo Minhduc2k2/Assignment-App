d8818ab8a6e84e8edfa628e5e833eb29231fcabd
Nguyễn Minh Đức <mingduc17082002@gmail.com>
2022-08-05 09:55:38 +0700

    Create ProjectSummary

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -1,12 +1,25 @@
 import { useDocument } from "../../hooks/useDocument";
 import { useParams } from "react-router-dom";
 import "./Project.css";
+import ProjectSummary from "./components/ProjectSummary/ProjectSummary";
+import ProjectComments from "./components/ProjectComments/ProjectComments";
 
 function Project() {
   const params = useParams();
   const { document, error } = useDocument("projects", params.id);
   console.log(document);
-  return <div>{error && <div className="error">{error}</div>}</div>;
+  return (
+    <div>
+      {error && <div className="error">{error}</div>}
+      {!document && <div className="loading">Loading...</div>}
+      {document && (
+        <div className="project-details">
+          <ProjectSummary project={document} />
+          <ProjectComments project={document} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Project;
